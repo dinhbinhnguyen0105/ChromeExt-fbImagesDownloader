f73f7febe9147c6cc392c6c0a9af8790d66e693b
dinhbinh nguyen <49284335+dinhbinhnguyen0105@users.noreply.github.com>
2022-12-23 09:46:19 +0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,20 @@
 console.log('contentScript.js');
 chrome.runtime.onMessage.addListener((request, sender, sendRes) => {
-    const imagesElm = document.querySelectorAll('img[referrerpolicy="origin-when-cross-origin"]');
+    const dialogs = document.querySelectorAll('div[role="dialog"]');
+    let dialog = null;
+    dialogs.forEach(_dialog => {
+        if(_dialog.offsetWidth > 0) {
+            dialog = _dialog;
+        };
+    })
+
+    const imagesElm = dialog.querySelectorAll('img[referrerpolicy="origin-when-cross-origin"]');
     if (imagesElm.length > 0) {
         const imagesSrc = [];
         imagesElm.forEach((image) => {
             const srcAtt = image.getAttribute('src');
             if(!imagesSrc.includes(srcAtt)) 
-            imagesSrc.push(srcAtt);
+            imagesSrc.push(srcAtt)
         });
         imagesSrc.forEach(imageSrc => {
             downloadImage(imageSrc, getName(imageSrc));
